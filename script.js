68e5081a5b3357fdaabdc2cc36b1ebad85eab247
mn1645322-cmd <mn1645322@gmail.com>
2026-08-07 22:09:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+// ALVORA Clothing
 
+let cart = [];
+
+function addToCart(productName, price) {
+    cart.push({
+        name: productName,
+        price: price
+    });
+
+    alert(productName + " تمت إضافته إلى السلة");
+}
+
+function viewCart() {
+    if (cart.length === 0) {
+        alert("السلة فارغة");
+        return;
+    }
+
+    let message = "المنتجات في السلة:\n\n";
+    let total = 0;
+
+    cart.forEach(function(item) {
+        message += item.name + " - " + item.price + " جنيه\n";
+        total += item.price;
+    });
+
+    message += "\nإجمالي السعر: " + total + " جنيه";
+
+    alert(message);
+}
+
+console.log("ALVORA Website Ready");
